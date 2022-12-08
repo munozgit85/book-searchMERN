7462098a05e4f38d6132714248645281c3fd9d5c
Racquel Munoz <racquelg85@hotmail.com>
2022-12-07 20:01:06 -0600

    add config

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/book-searchMERN",
+  process.env.MONGODB_URI || "mongodb://67.11.99.167/32/book-searchMERN",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
