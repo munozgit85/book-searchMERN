9eef29c410dedc9b9e85da15ce2550ed22c9bbb6
Racquel Munoz <racquelg85@hotmail.com>
2022-12-07 18:40:45 -0600

    added changes to mongoose

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/book-searchMERN",
+  process.env.MONGODB_URI || "mongodb://localhost:27017/book-searchMERN.books",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
